df61478d883c1e58cd9459c9cdea290261debdb4
heipiao233 <heipiao233@outlook.com>
2023-04-30 08:01:43 +0800

    Add Quilt mod 2

--- src/main/java/net/heipiao/qpac/ExampleMod.java
+++ src/main/java/net/heipiao/qpac/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.example_mod;
+package net.heipiao.qpac;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
--- src/main/java/net/heipiao/qpac/mixin/TitleScreenMixin.java
+++ src/main/java/net/heipiao/qpac/mixin/TitleScreenMixin.java
@@ -1,11 +1,12 @@
-package com.example.example_mod.mixin;
+package net.heipiao.qpac.mixin;
 
-import com.example.example_mod.ExampleMod;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.heipiao.qpac.ExampleMod;
+import net.minecraft.client.gui.screen.TitleScreen;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
